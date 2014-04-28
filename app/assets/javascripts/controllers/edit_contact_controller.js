95ddab23da755434aaeeb171daf0106f3877896c
John Cech & Corey Norris <student@epicodus.com>
2014-04-28 10:31:01 -0700

    add edit functionality to contact model

---
+++ app/assets/javascripts/controllers/edit_contact_controller.js
@@ -0,0 +1,16 @@
+WikipagesWithEmber.EditContactController = Ember.ObjectController.extend({
+  actions: {
+    updateContact: function() {
+      var model = this.get('model');
+      var controller = this;
+
+      model.save()
+      .then(function() {
+        controller.transitionToRoute('contact', model);
+      })
+      .catch(function() {
+        alert("Please fix the noted problems.")
+      });
+    }
+  }
+});
---
+++ app/assets/javascripts/routes/edit_contact_route.js
@@ -0,0 +1,5 @@
+WikipagesWithEmber.EditContactRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('contact', params.id)
+  }
+});
